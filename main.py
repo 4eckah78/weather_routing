0f53f88d079e28a3a211fcb24850bd2c9a26c1a0
georgiy.chaklyarov <georgiy.chaklyarov@lgepartner.com>
2021-10-14 23:01:46 +0300

    Change hex coords, fix hexmap drawing algorithm
    - Now hex coordinates are stored in more convenient way
    - Hexmap drawing algorithm works more correct (without strange doubled hexes)
    - Also made polygon coordinates as integer for greater clarity

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import PIL.ImageDraw as ImageDraw
 import PIL.Image as Image
+import math
 
 height = 480
 width = 640
-a = 50
+a = 15
+polygons_file = 'polygons.txt'
 
 
 def generate_polygon(x0, y0, r_range, n_range):
@@ -18,11 +20,11 @@ def generate_polygon(x0, y0, r_range, n_range):
     while fi <= 2 * pi:
         new_x = int(r * np.cos(fi)) + x0
         new_y = int(r * np.sin(fi)) + y0
-        if 0 <= new_x <= width:
+        if 0 <= new_x < width:
             x.append(new_x)
         else:
             x.append(width) if 0 <= new_x else x.append(0)
-        if 0 <= new_y <= height:
+        if 0 <= new_y < height:
             y.append(new_y)
         else:
             y.append(height) if 0 <= new_y else y.append(0)
@@ -40,24 +42,24 @@ def generate_N_polygons(N, r_range, sides_range):
 
     with open("polygons.txt", 'w') as output_file:
         for _ in range(N):
-            x0 = np.random.rand() * width
-            y0 = np.random.rand() * height
+            x0 = int(np.random.rand() * width)
+            y0 = int(np.random.rand() * height)
             x, y = generate_polygon(x0, y0, r_range, sides_range)
             points = list(zip(x, y))
             draw.polygon((points), fill=1)
             points = ",".join([str(x) + "," + str(y) for x, y in zip(x, y)])
             output_file.write(points + '\n')
-    # image.show()
+    image.show()
 
 
 def pixel_to_flat_hex(x, y):
-    col = x * 2 / 3 / a
     row = y * 2 / np.sqrt(3) / a
-    col, row = cube_to_doubleheight(*cube_round(*doubleheight_to_cube(col, row)))
-    return col, row
+    col = x * 2 / 3 / a
+    row, col = cube_to_doubleheight(*cube_round(*doubleheight_to_cube(row, col)))
+    return row, col
 
 
-def doubleheight_to_cube(col, row):
+def doubleheight_to_cube(row, col):
     x = col
     z = (row - col) / 2
     y = -x - z
@@ -65,12 +67,12 @@ def doubleheight_to_cube(col, row):
 
 
 def cube_to_doubleheight(x, y, z):
-    col = x
     row = 2 * z + x
-    return col, row
+    col = x
+    return row, col
 
 
-def doubleheight_to_pixel(col, row):
+def doubleheight_to_pixel(row, col):
     x = a * 3 / 2 * col
     y = a * np.sqrt(3) / 2 * row
     return x, y
@@ -95,54 +97,53 @@ def cube_round(x, y, z):
     return rx, ry, rz
 
 
-if __name__ == "__main__":
-    hex_width = int(width / (2 * a)) + 100
-    hex_height = int(height / (np.sqrt(3) * a)) + 100
-    hex_map = np.zeros((hex_height, hex_width))
-    print(hex_map.shape)
-    with open("polygons.txt", 'r') as f:
-        polygons = [list(map(float, pol.rstrip().split(','))) for pol in f.readlines()]
+def read_polygons_from(filename):
+    with open(filename, 'r') as f:
+        pols = [list(map(int, pol.rstrip().split(','))) for pol in f.readlines()]
+    return pols
 
-    hex_polygons = []
-    for polygon in polygons:
-        list = []
+
+def from_pixel_to_hex_polygons(pols):
+    hex_pols = []
+    for polygon in pols:
+        hex_pol = []
         for i in range(0, len(polygon), 2):
-            col, row = pixel_to_flat_hex(polygon[i], polygon[i + 1])
-            list.append(col)
-            list.append(row)
-            # print(f'hex({col}, {row}) --> hex_map[{row}, {col // 2}]')
+            row, col = pixel_to_flat_hex(polygon[i], polygon[i + 1])
             hex_map[row, col // 2] = 1
-            x, y = doubleheight_to_pixel(col, row)
-            hex_polygons.append(list)
-            print(int(polygon[i]), int(polygon[i+1]), " --> ", col, row)
-            print(col, row, " --> ", x, y)
-        hex_polygons.append(list)
+            hex_pol.append([row, col])
+            # x, y = doubleheight_to_pixel(row, col)
+            # print(int(polygon[i]), int(polygon[i + 1]), " --> ", row, col)
+            # print(row, col, " --> ", x, y)
+        hex_pols.append(hex_pol)
+    return hex_pols
 
-    print(hex_map)
 
-    image = Image.new("1", (width, height), color=0)
+def show_hex_image(width, height, hex_width, hex_height, a):
+    image = Image.new("1", (width, height), color=1)
     draw = ImageDraw.Draw(image)
-    i = 0
-    color = 1
-    for x in range(0, width + a, int(3 * a / 2)):
-        for y in range(0, height + a, int(np.sqrt(3) * a)):
-            col, row = pixel_to_flat_hex(x, y)
-            if hex_map[row, col // 2] == 1:
-                color = 0
-            if i % 2 == 0:
-                draw.regular_polygon((x, y, a), 6, fill=color, outline=0)
-            else:
-                draw.regular_polygon((x, y + np.sqrt(3) * a / 2, a), 6, fill=color, outline=0)
-            color = 1
 
-        i += 1
-    # for polygon in hex_polygons:
-    #     for i in range(0, len(polygon), 2):
-    #         x, y = doubleheight_to_pixel(col, row)
-    #
-    #         draw.regular_polygon((x, y, a), 6, fill=0)
-
+    for j in range(hex_height):
+        for i in range(hex_width):
+            x0 = 3 * a * i
+            if j % 2 != 0:
+                x0 = 3 * a * (2 * i + 1) / 2
+            y0 = j * np.sqrt(3) * a / 2
+            row, col = pixel_to_flat_hex(x0, y0)
+            color = 0 if hex_map[row, col // 2] == 1 else 1
+            draw.regular_polygon((x0, y0, a), 6, fill=color, outline=0)
     image.show()
+
+
+if __name__ == "__main__":
+    hex_width = math.ceil(width / (3 * a))
+    hex_height = math.ceil(height / (np.sqrt(3) * a)) * 2 + 1
+    hex_map = np.zeros((hex_height, hex_width), dtype=np.uint8)
+
+    polygons = read_polygons_from(polygons_file)
+
+    hex_polygons = from_pixel_to_hex_polygons(polygons)
+
+    show_hex_image(width, height, hex_width, hex_height, a)
 
     # generate_N_polygons(10, [10, 100], [3, 6])
     # on average 3 heptagon (polygon with 7 sides) per 100,000 cases
